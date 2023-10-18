4a60f15bbd20e1f7c359e959dc37650fda96228e
mudasirCs <mudasir.cs47@gmail.com>
2023-10-18 13:15:19 +0500

    Item addition added, with some bugs

--- src/displayHandler.js
+++ src/displayHandler.js
@@ -1,16 +1,19 @@
 import createItem from "./item";
 import katana from "./katana.png";
+import createProject from "./project";
 import shuriken from "./shurikenAdd.svg";
 import "./styles.css";
 import tooltip from "./tooltip.svg";
 
 export default function createDisplayHandler() {
   const main = document.querySelector("main");
-
+  //later will use persistance layer, this is temporary
+  let proj;
   //general implementation
   //split into create proj, add items, remove items
   //create loadProject function to load from persistance layer
-  function renderProject(proj) {
+  function renderProject(project) {
+    proj = project;
     const projectContainer = document.createElement("ul");
     projectContainer.classList.add("project-container");
 
@@ -37,31 +40,41 @@ export default function createDisplayHandler() {
 
     main.appendChild(projectContainer);
 
-    activateAddItemButtons();
+    activateAddItemButtons(proj);
+    initFormActions((item) => {
+      proj.addItem(item);
+    });
   }
 
-  /*  title,
-  description,
-  notes,
-  dueDate,
-  priority,
-  completedStatus
- */
+  function renderItemListRefresh() {
+    const projectContainer = document.querySelector(".project-container");
+
+    const listItems = projectContainer.querySelectorAll("li.list-item");
+    listItems.forEach((item) => {
+      projectContainer.removeChild(item);
+    });
+
+    console.log("Entered into the den");
+    const addBtn = document.querySelector(".add");
+
+    proj.itemsList.forEach((element) => {
+      const item = renderListItem(element);
+      projectContainer.insertBefore(item, addBtn);
+    });
+  }
 
   function activateAddItemButtons() {
     const addBtns = document.querySelectorAll(".project-container img.add");
     addBtns.forEach((addBtn) => {
       addBtn.addEventListener("click", (e) => {
-        renderItemPopUp((item) => {
-          proj.projList.addItem(item);
-        });
+        const dialog = document.querySelector("dialog");
+        dialog.showModal();
       });
     });
   }
 
-  function renderItemPopUp(callBackFn) {
-    const dialog = document.querySelector(".project-container dialog");
-
+  function initFormActions(callBackFn) {
+    const dialog = document.querySelector("dialog");
     const form = document.querySelector("dialog form");
 
     const itemTitle = form.querySelector("#item-title");
@@ -71,29 +84,55 @@ export default function createDisplayHandler() {
     const itemPriority = form.querySelector("#item-priority");
     const completedStatus = form.querySelector("#completed-status");
 
+    const submitBtn = document.querySelector(".submit");
+
     //all validation rules should be made
-    form.addEventListener("submit", (e) => {
+    submitBtn.addEventListener("click", (e) => {
       e.preventDefault();
 
-      const title = itemTitle.value;
-      const description = itemDescription.value;
-      const notes = itemNotes.value;
-      const dueDate = itemDueDate.value;
-      const priority = itemPriority.value;
-      const isCompleted = completedStatus.checked;
+      // const title = itemTitle.value;
+      // const description = itemDescription.value;
+      // const notes = itemNotes.value;
+      // const dueDate = itemDueDate.value;
+      // const priority = itemPriority.value;
+      // const isCompleted = completedStatus.checked;
 
-      const item = createItem(
-        title,
-        description,
-        notes,
-        dueDate,
-        priority,
-        isCompleted
+      // const item = createItem(
+      //   title,
+      //   description,
+      //   notes,
+      //   dueDate,
+      //   priority,
+      //   isCompleted
+      // );
+
+      itemTitle.value = "";
+      itemDescription.value = "";
+      itemNotes.value = "";
+      //need to fix date intake/conversion
+      itemDueDate.value = "";
+      itemPriority.value = "";
+      completedStatus.value = false;
+
+      let item = createItem(
+        "Template Title",
+        "Template description, just some random text that needs to be added",
+        "These are the template notes",
+        "10,10,2025",
+        "Medium",
+        true
       );
+
+      callBackFn(item);
+      //update list again
+      renderItemListRefresh();
+      dialog.close();
     });
 
-    form.addEventListener("cancel", (e) => {
-      dialog.close;
+    const cancelBtn = document.querySelector(".cancel");
+    cancelBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      dialog.close();
     });
   }
 
@@ -130,7 +169,23 @@ export default function createDisplayHandler() {
     const priorityDiv = document.createElement("div");
     priorityDiv.classList.add("list-item-priority-container");
 
-    for (let i = item.priority; i <= 3; i++) {
+    let priority;
+
+    switch (item.priority) {
+      case "High":
+        priority = 1;
+        break;
+      case "Medium":
+        priority = 2;
+        break;
+      case "Low":
+        priority = 3;
+        break;
+      default:
+        priority = 0;
+        break;
+    }
+    for (let i = priority; i <= 3; i++) {
       const priorityImg = document.createElement("img");
       priorityImg.classList.add("list-item-priority");
       priorityImg.src = katana;
@@ -143,8 +198,7 @@ export default function createDisplayHandler() {
     checkbox.classList.add("list-item-status");
     checkbox.checked = item.completedStatus;
 
-    //if checkbox is checked
-
+    li.appendChild(headDiv);
     li.appendChild(description);
     li.appendChild(priorityDiv);
     li.appendChild(checkbox);
